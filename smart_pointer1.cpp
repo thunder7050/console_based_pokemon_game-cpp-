7917b105951571223f987d0adfe32beb9f145a02
thunder7050 <ap0383683@gmail.com>
2026-01-31 22:47:46 +0530

    Delete smart_pointer1.cpp

--- smart_pointer1.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <memory>
-using namespace std;
-
-int main() {
-    unique_ptr<int> ptr1(new int(12));    //variable ptr1,value 12
-    cout << *ptr1 << endl;               // since ptr1 is a pointer we use * to access the value 
-    return 0;
-}
